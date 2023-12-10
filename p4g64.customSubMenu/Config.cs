9e54d8e550bb9c48b9cb22d93238ff9bdab90dce
Pixelguin <11445611+Pixelguin@users.noreply.github.com>
2023-12-10 12:33:57 -0800

    Improve Mod Menu warning

--- p4g64.customSubMenu/Config.cs
+++ p4g64.customSubMenu/Config.cs
@@ -6,7 +6,7 @@ namespace p4g64.customSubMenu.Configuration
     public class Config : Configurable<Config>
     {
         [DisplayName("Show Mod Menu")]
-        [Description("If enabled you will be able to use the mod menu (you still need to enable it in the in-game options).\nNote that mod menu is only meant for mod developers, if you use it and break your save you won't get help!")]
+        [Description("If enabled you will be able to access the Mod Menu (make sure to also enable Mod Menu in the in-game options).\nThe Mod Menu is for mod developers and dataminers - it is NOT a \"cheats menu\" for players!\nIf you use it and break your save, you won't get help!")]
         [DefaultValue(false)]
         public bool ShowModMenu { get; set; } = false;
     }
